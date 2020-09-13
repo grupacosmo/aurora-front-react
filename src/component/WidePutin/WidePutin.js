c8c47af24ac20396c3fc51dfe5a1c66768cddb91
borchowiec <borchowiecpatryk@gmail.com>
2020-09-13 19:03:15 +0200

    Prepared template.

--- src/component/WidePutin/WidePutin.js
+++ src/component/WidePutin/WidePutin.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./style.css";
 
 import memeMusic from '../../assets/sound/wide-putin.mp3';
 
 export default function WidePutin() {
+
+  const [putin, setPutin] = useState(false);
+
   return (
     <>
-      <audio
-        autoPlay={true}
-        controls={false}>
-        <source type="audio/mp3" src={memeMusic} />
-      </audio>
-      <img src="https://steamuserimages-a.akamaihd.net/ugc/1477696595663509341/CF90E4FB3A04D84AD84C01D15D3AC08AF63C1053/?imw=506&imh=284&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
-           alt="wide-putin" id="widePutinGif"/>
-      <h1>Aurora</h1>
-      <h2>Home section</h2>
-      <h3>Flipper przemierza PK</h3>
+      <p>Kliknij play</p>
+      <div onPlay={() => setPutin(true)}>
+        <audio
+          autoPlay={false}
+          controls={true}
+          id="memeAudio">
+          <source type="audio/mp3" src={memeMusic}/>
+        </audio>
+      </div>
+      {
+        putin ?
+          <>
+            <img src="https://steamuserimages-a.akamaihd.net/ugc/1477696595663509341/CF90E4FB3A04D84AD84C01D15D3AC08AF63C1053/?imw=506&imh=284&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
+                 alt="wide-putin" id="widePutinGif"/>
+            <h1>Aurora</h1>
+            <h2>Home section</h2>
+            <h3>Flipper przemierza PK</h3>
+          </> : <></>
+      }
     </>
   );
 }
