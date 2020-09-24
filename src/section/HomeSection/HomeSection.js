2ad6eed4e3dbb0df4dd89cd934c7071f218357d6
borchowiec <borchowiecpatryk@gmail.com>
2020-09-24 22:17:05 +0200

    Improved responsivness

--- src/section/HomeSection/HomeSection.js
+++ src/section/HomeSection/HomeSection.js
@@ -4,22 +4,23 @@ import Title from "antd/lib/typography/Title";
 
 import cubesat from "../../assets/img/cubesat.svg";
 import "./cubesat.css"
+import "./style.css"
 
 const {Content} = Layout;
 
 export default function HomeSection() {
   return (
-    <Layout>
+    <Layout id="homeSection">
       <Content style={{backgroundColor: "#1f1f1f", color: "white"}}>
         <Row align="middle" style={{padding: "100px 5px", textAlign: "center"}}>
-          <Col span={24} md={12}>
+          <Col span={24} sm={14} md={12}>
             <div style={{textAlign: "left", display: "inline-block"}}>
-              <Title style={{color: "#1890ff", fontSize: "100px", fontWeight: "normal"}}>Aurora</Title>
+              <Title style={{color: "#1890ff", fontSize: "100px", fontWeight: "normal"}} className="title">Aurora</Title>
               <h1 style={{color: "#bfbfbf", fontWeight: "lighter", fontSize: "40px", marginBottom: 0}}>Catchy slogan</h1>
               <h2 style={{color: "#fafafa", fontWeight: "lighter"}}>Lorem ipsum dolor sit amet</h2>
             </div>
           </Col>
-          <Col span={24} md={12} style={{textAlign: "center"}}>
+          <Col span={24} sm={10} md={12} style={{textAlign: "center"}}>
             <img id="cubesatImg" src={cubesat} alt="cubesat" style={{width: "90%"}} />
           </Col>
         </Row>
