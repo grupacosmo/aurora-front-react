fdbf16e016557f434f683376717a445ab7b3c84b
borchowiec <borchowiecpatryk@gmail.com>
2020-09-24 21:55:45 +0200

    Created home section.

--- src/section/HomeSection/HomeSection.js
+++ src/section/HomeSection/HomeSection.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import WidePutin from "../../component/WidePutin/WidePutin";
+import {Col, Layout, Row} from "antd";
+import Title from "antd/lib/typography/Title";
+
+import cubesat from "../../assets/img/cubesat.svg";
+import "./cubesat.css"
+
+const {Content} = Layout;
 
 export default function HomeSection() {
   return (
-    <>
-      <WidePutin />
-    </>
+    <Layout>
+      <Content style={{backgroundColor: "#1f1f1f", color: "white"}}>
+        <Row align="middle" style={{padding: "100px 5px", textAlign: "center"}}>
+          <Col span={24} md={12}>
+            <div style={{textAlign: "left", display: "inline-block"}}>
+              <Title style={{color: "#1890ff", fontSize: "100px", fontWeight: "normal"}}>Aurora</Title>
+              <h1 style={{color: "#bfbfbf", fontWeight: "lighter", fontSize: "40px", marginBottom: 0}}>Catchy slogan</h1>
+              <h2 style={{color: "#fafafa", fontWeight: "lighter"}}>Lorem ipsum dolor sit amet</h2>
+            </div>
+          </Col>
+          <Col span={24} md={12} style={{textAlign: "center"}}>
+            <img id="cubesatImg" src={cubesat} alt="cubesat" style={{width: "90%"}} />
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
   );
 }
