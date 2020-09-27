d03c21e15b5af2874ab4c5cf10330f9b47e433fa
blaze <blazejos1230@gmail.com>
2020-09-28 00:26:39 +0200

    Title animation and polished design

--- src/component/ExperimentTile/ExperimentTile.js
+++ src/component/ExperimentTile/ExperimentTile.js
@@ -1,23 +1,24 @@
 import React, {useEffect, useState} from "react";
 import "./ExperimentTile.css"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faUserAstronaut} from '@fortawesome/free-solid-svg-icons'
 
 const ExperimentTile = (props) => {
     const [isRendered, setIsRendered] = useState(false)
     const [opacity, setOpacity] = useState(0)
     const [translate, setTranslate] = useState("100px")
 
+    const scrollHeight = 1 //Będzie to wysokość od której karty bedą się pojawiać
+
 
     useEffect(() => {
         const handleScroll = () => {
             const currentScrollY = window.scrollY;
-            if (currentScrollY > 24 && !isRendered) {
+            if (currentScrollY > scrollHeight && !isRendered) {
                 setIsRendered(true);
                 setOpacity(1)
                 setTranslate("0px")
             }
-            if (currentScrollY < 24 && isRendered) {
+            if (currentScrollY < scrollHeight && isRendered) {
                 setIsRendered(false);
                 setOpacity(0)
                 setTranslate("100px")
@@ -38,8 +39,8 @@ const ExperimentTile = (props) => {
 
     return (
         <div id="tileContainer" style={style}>
-            <FontAwesomeIcon style={{fontSize: 80}} icon={faUserAstronaut}/>
-            <h2 style={{fontSize: 30, fontWeight: 500}}>RANDOM TITLE</h2>
+            <FontAwesomeIcon style={{fontSize: 80,color:"#22267b"}} icon={props.data.icon}/>
+            <h2>{props.data.title}</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue facilisis augue, eu consequat magna
                 rhoncus in. Etiam tincidunt eros vitae ultricies efficitur. Maecenas varius vitae dui vehicula dapibus.
                 Vivamus a finibus odio, eget ultrices lectus. Nam nibh eros, consectetur a nulla vel, luctus venenatis
--- src/section/Experiment/Experiment.js
+++ src/section/Experiment/Experiment.js
@@ -1,29 +1,91 @@
-import React,{useRef} from "react";
+import React, {useEffect, useState} from "react";
 import "./Experiment.css"
 import ExperimentTile from "../../component/ExperimentTile/ExperimentTile";
 import {Row, Col} from 'antd';
+import {faSitemap, faSatellite, faSatelliteDish, faRobot} from '@fortawesome/free-solid-svg-icons'
 
+const tileData = [
+    {
+        icon: faSitemap,
+        title: "Architektura",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        icon: faSatellite,
+        title: "Zasilanie",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        icon: faSatelliteDish,
+        title: "Komunikacja",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        icon: faRobot,
+        title: "Sztuczna Inteligencja",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
+]
 
 const Experiment = () => {
-    const el = useRef(null);
+    const [isRendered, setIsRendered] = useState(false)
+    const [opacity, setOpacity] = useState(0)
+    const [translate, setTranslate] = useState("100px")
+
+    const titleScrollHeight = 1 //Będzie to wysokość od której tytuł bedą się pojawiać
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+            if (currentScrollY > titleScrollHeight && !isRendered) {
+                setIsRendered(true);
+                setOpacity(1)
+                setTranslate("0px")
+            }
+            if (currentScrollY < titleScrollHeight && isRendered) {
+                setIsRendered(false);
+                setOpacity(0)
+                setTranslate("100px")
+            }
+        }
+        window.addEventListener("scroll", handleScroll, {passive: true});
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [isRendered])
+
+    const style = {
+        opacity: opacity,
+        transform: `translate(0,${translate})`,
+        transition: "opacity 1s, transform 1s"
+    }
+
     return (
         <div id="mainDiv">
-            <h1 className="Title">THE PILLARS OF DIGITAL</h1>
+            <div id="titleContainer" style={style}>
+                <Row>
+                    <Col span={24}>
+                        <h1 id="title">Experymenty Aurory</h1>
+                    </Col>
+                    <Col span={12} offset={6}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue facilisis augue, eu
+                            consequat magna rhoncus in. Etiam tincidunt eros vitae ultricies efficitur</p>
+                    </Col>
+                </Row>
+            </div>
             <Row>
                 <Col span={18} offset={3}>
                     <div id="tilesContainer">
                         <Row gutter={[32, 24]}>
                             <Col span={12} className="gutter-row">
-                                <ExperimentTile translate={"-"} ref={el}/>
+                                <ExperimentTile translate={"-"} data={tileData[0]}/>
                             </Col>
                             <Col span={12} className="gutter-row">
-                                <ExperimentTile translate={""}/>
+                                <ExperimentTile translate={""} data={tileData[1]}/>
                             </Col>
                             <Col span={12} className="gutter-row">
-                                <ExperimentTile translate={"-"}/>
+                                <ExperimentTile translate={"-"} data={tileData[2]}/>
                             </Col>
                             <Col span={12} className="gutter-row">
-                                <ExperimentTile translate={""}/>
+                                <ExperimentTile translate={""} data={tileData[3]}/>
                             </Col>
                         </Row>
                     </div>
