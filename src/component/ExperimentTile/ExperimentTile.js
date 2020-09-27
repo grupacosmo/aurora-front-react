dd6a774a8788a846028c83045b3751b03757c339
blaze <blazejos1230@gmail.com>
2020-09-27 22:07:15 +0200

    Added animation to the Tile component

--- src/component/ExperimentTile/ExperimentTile.js
+++ src/component/ExperimentTile/ExperimentTile.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./ExperimentTile.css"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faUserAstronaut, faSpaceShuttle} from '@fortawesome/free-solid-svg-icons'
+import {faUserAstronaut} from '@fortawesome/free-solid-svg-icons'
 
-const ExperimentTile = () => {
+const ExperimentTile = (props) => {
+    const [isRendered, setIsRendered] = useState(false)
+    const [opacity, setOpacity] = useState(0)
+    const [translate, setTranslate] = useState("100px")
+
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+            if (currentScrollY > 24 && !isRendered) {
+                setIsRendered(true);
+                setOpacity(1)
+                setTranslate("0px")
+            }
+            if (currentScrollY < 24 && isRendered) {
+                setIsRendered(false);
+                setOpacity(0)
+                setTranslate("100px")
+            }
+
+        }
+
+        window.addEventListener("scroll", handleScroll, {passive: true});
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [isRendered])
+
+
+    const style = {
+        opacity: opacity,
+        transform: `translate(${props.translate}${translate})`,
+        transition: "opacity 1s, transform 1s"
+    }
+
     return (
-        <div id="tileContainer">
+        <div id="tileContainer" style={style}>
             <FontAwesomeIcon style={{fontSize: 80}} icon={faUserAstronaut}/>
-            <h2 style={{fontSize:30, fontWeight:500}}>RANDOM TITLE</h2>
+            <h2 style={{fontSize: 30, fontWeight: 500}}>RANDOM TITLE</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue facilisis augue, eu consequat magna
                 rhoncus in. Etiam tincidunt eros vitae ultricies efficitur. Maecenas varius vitae dui vehicula dapibus.
                 Vivamus a finibus odio, eget ultrices lectus. Nam nibh eros, consectetur a nulla vel, luctus venenatis
--- src/section/Experiment/Experiment.js
+++ src/section/Experiment/Experiment.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React,{useRef} from "react";
 import "./Experiment.css"
 import ExperimentTile from "../../component/ExperimentTile/ExperimentTile";
 import {Row, Col} from 'antd';
 
 
 const Experiment = () => {
+    const el = useRef(null);
     return (
         <div id="mainDiv">
             <h1 className="Title">THE PILLARS OF DIGITAL</h1>
-            <div id="tilesContainer">
-                <Row gutter={[16,24]}>
-                    <Col span={12} className="gutter-row"><ExperimentTile/></Col>
-                    <Col span={12} className="gutter-row"><ExperimentTile/></Col>
-                    <Col span={12} className="gutter-row"><ExperimentTile/></Col>
-                    <Col span={12} className="gutter-row"><ExperimentTile/></Col>
-                </Row>
-            </div>
+            <Row>
+                <Col span={18} offset={3}>
+                    <div id="tilesContainer">
+                        <Row gutter={[32, 24]}>
+                            <Col span={12} className="gutter-row">
+                                <ExperimentTile translate={"-"} ref={el}/>
+                            </Col>
+                            <Col span={12} className="gutter-row">
+                                <ExperimentTile translate={""}/>
+                            </Col>
+                            <Col span={12} className="gutter-row">
+                                <ExperimentTile translate={"-"}/>
+                            </Col>
+                            <Col span={12} className="gutter-row">
+                                <ExperimentTile translate={""}/>
+                            </Col>
+                        </Row>
+                    </div>
+                </Col>
+            </Row>
         </div>
     )
 }
