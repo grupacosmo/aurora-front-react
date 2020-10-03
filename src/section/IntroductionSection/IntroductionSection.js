2da1e3fff26016546cb3907ceca1c342d0579e42
blaze <blazejos1230@gmail.com>
2020-10-03 16:52:37 +0200

    Arrow icon and animated background

--- src/section/IntroductionSection/IntroductionSection.js
+++ src/section/IntroductionSection/IntroductionSection.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import {Row,Col} from "antd"
 import {faAngleDown} from '@fortawesome/free-solid-svg-icons'
 
 import "./IntroductionSection.css"
+import "./Animation.css"
 import city from "../../assets/img/city.jpg"
+import worldMap from "../../assets/img/worldMap.png"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 const IntroductionSection = () => {
+    const [image,setImage] = useState(city)
+    const [select,setSelect] = useState(1)
+    const [photoClass,setPhotoClass] = useState("")
+
+    const pictures = [city,worldMap]
+
+    useEffect(() =>{
+        setPhotoClass("photoFadeIn")
+        setTimeout(() =>{
+            setPhotoClass("photoFadeOut")
+        },9000)
+        setTimeout(() =>{
+            setImage(pictures[select])
+            setSelect((prev) =>{
+                if(prev > pictures.length - 2){
+                    return 0
+                }
+                return prev + 1
+            })
+        },10000)
+    },[select])
+
     return (
         <div id="IntroSection">
             <div id="PhotoContainer">
                 <img id="photo"
-                     src={city}
+                     className={photoClass}
+                     src={image}
                      alt="city"
                 />
             </div>
