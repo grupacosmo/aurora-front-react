2a99387fa139f6e2fee00b7bf5439437e794d55e
blaze <blazejos1230@gmail.com>
2020-10-06 00:11:11 +0200

    Polished background animation

--- src/section/IntroductionSection/IntroductionSection.js
+++ src/section/IntroductionSection/IntroductionSection.js
@@ -1,45 +1,59 @@
-import React,{useState,useEffect} from "react";
-import {Row,Col} from "antd"
+import React, {useState, useEffect, useRef} from "react";
+import {Row, Col} from "antd"
 import {faAngleDown} from '@fortawesome/free-solid-svg-icons'
 
 import "./IntroductionSection.css"
 import "./Animation.css"
 import city from "../../assets/img/city.jpg"
-import worldMap from "../../assets/img/worldMap.png"
+import city2 from "../../assets/img/city2.jpg"
+import europe from "../../assets/img/europe.jpg"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 const IntroductionSection = () => {
-    const [image,setImage] = useState(city)
-    const [select,setSelect] = useState(1)
-    const [photoClass,setPhotoClass] = useState("")
+    const [topPhotoPointer, setTopPhotoPointer] = useState(0)
+    const [bottomPhotoPointer, setBottomPhotoPointer] = useState(1)
+    const [toggle, setToggle] = useState(true)
+    const topPhotoRef = useRef()
 
-    const pictures = [city,worldMap]
+    const pictures = [city, europe, city2]
 
-    useEffect(() =>{
-        setPhotoClass("photoFadeIn")
-        setTimeout(() =>{
-            setPhotoClass("photoFadeOut")
-        },9000)
-        setTimeout(() =>{
-            setImage(pictures[select])
-            setSelect((prev) =>{
-                if(prev > pictures.length - 2){
-                    return 0
-                }
-                return prev + 1
-            })
-        },10000)
-    },[select])
+    useEffect(() => {
+        if (toggle) {
+            setTimeout(() => {
+                setTopPhotoPointer((prev) => {
+                    if (prev >= pictures.length - 2) {
+                        return (prev + 2) % pictures.length
+                    }
+                    return prev + 2
+                })
+                setToggle(prev => !prev)
+            }, 10000)
+        } else {
+            setTimeout(() => {
+                setBottomPhotoPointer((prev) => {
+                    if (prev >= pictures.length - 2) {
+                        return (prev + 2) % pictures.length
+                    }
+                    return prev + 2
+                })
+
+                //Usuwanie i dodawanie tej klasy startuje od nowa animacje co zsynchronizować zmienianie się zdjęć
+                //właśnie z tą animacją
+                topPhotoRef.current.classList.remove("topPhoto")
+                topPhotoRef.current.classList.add("topPhoto")
+
+                setToggle(prev => !prev)
+            }, 10000)
+        }
+
+    }, [toggle])
 
     return (
         <div id="IntroSection">
             <div id="PhotoContainer">
-                <img id="photo"
-                     className={photoClass}
-                     src={image}
-                     alt="city"
-                />
+                <img id="photo" className="bottomPhoto" src={pictures[bottomPhotoPointer]} alt="bottomPhoto"/>
+                <img id="photo" ref={topPhotoRef} className="topPhoto" src={pictures[topPhotoPointer]} alt="topPhoto"/>
             </div>
             <div id="Background"/>
             <div id="ContentContainer">
