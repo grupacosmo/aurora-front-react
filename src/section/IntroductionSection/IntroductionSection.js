165a9a3d53e7227353c1fe1c15a04e4a12a82aff
blaze <blazejos1230@gmail.com>
2020-10-09 22:58:37 +0200

    Responsive design

--- src/section/IntroductionSection/IntroductionSection.js
+++ src/section/IntroductionSection/IntroductionSection.js
@@ -4,7 +4,7 @@ import {Link} from "react-scroll";
 import {faAngleDown} from '@fortawesome/free-solid-svg-icons'
 
 import "./IntroductionSection.css"
-import Square from "../../component/Square/Square";
+
 import Comet from "../../component/Comet/Comet";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
@@ -12,11 +12,6 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 const IntroductionSection = (props) => {
     return (
         <div id="IntroSection">
-            {/*<div id="SquaresContainer">*/}
-            {/*    <Square/>*/}
-            {/*    <Square/>*/}
-            {/*    <Square/>*/}
-            {/*</div>*/}
             <div id="CometContainer">
                 <Comet/>
                 <Comet/>
@@ -24,34 +19,34 @@ const IntroductionSection = (props) => {
                 <Comet/>
                 <Comet/>
             </div>
-                <Row>
-                    <Col span={18} offset={3}>
-                        <div id="CardBox">
-                            <Row>
-                                <Col span={24}>
-                                    <h1>Cel Projektu</h1>
-                                </Col>
-                                <Col span={12} offset={6}>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue
-                                        facilisis
-                                        augue, eu
-                                        consequat magna rhoncus in. Etiam tincidunt eros vitae ultricies
-                                        efficitur</p>
-                                </Col>
-                            </Row>
-                        </div>
+            <Row>
+                <Col xl={{span:18, offset:3}} sm={{span:20, offset:2}} xs={{span:20, offset:2}}>
+                    <div id="CardBox">
                         <Row>
-                            <Col span={6} offset={9}>
-                                <Link to={props.target}
-                                      spy={true}
-                                      smooth={true}
-                                      duration={1000}
-                                      activeClass=" ">
-                                <FontAwesomeIcon className="IntroIcon" icon={faAngleDown}/></Link>
+                            <Col span={24}>
+                                <h1>Cel Projektu</h1>
+                            </Col>
+                            <Col md={{span:16, offset:4}} sm={{span:20, offset:2}} xs={{span:20, offset:2}}>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel ornare mi.
+                                    Vestibulum ut sapien ullamcorper, consectetur magna ac, auctor orci. Sed facilisis,
+                                    odio ac dignissim finibus, arcu magna facilisis nunc, vitae sodales nibh quam eget
+                                    nisi. Integer vel diam eu massa placerat fringilla. Vestibulum metus mi, viverra a
+                                    mattis blandit, luctus et massa. Praesent vestibulum efficitur egestas. Curabitur
+                                    posuere nibh augue, quis porttitor mi lobortis non. Pellentesque non bibendum purus.
+                                    Vestibulum eu enim vel tortor consequat egestas. Nunc sed vestibulum ante. Maecenas
+                                    egestas diam lacus, accumsan facilisis risus consectetur at. </p>
                             </Col>
                         </Row>
-                    </Col>
-                </Row>
+                    </div>
+                    <Link to={props.target}
+                          spy={true}
+                          smooth={true}
+                          duration={1000}
+                          activeClass=" ">
+                        <FontAwesomeIcon className="IntroIcon" icon={faAngleDown}/>
+                    </Link>
+                </Col>
+            </Row>
         </div>
     )
 }
