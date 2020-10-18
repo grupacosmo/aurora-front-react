7b4d9a8b0551e97fccb91884a2b93ad3517f174b
borchowiec <borchowiecpatryk@gmail.com>
2020-10-18 19:11:11 +0200

    Added intro section

--- src/view/LandingPage/LandingPage.js
+++ src/view/LandingPage/LandingPage.js
@@ -3,11 +3,13 @@ import HomeSection from "../../section/HomeSection/HomeSection";
 import Experiment from "../../section/Experiment/Experiment";
 import Footer from "../../section/Footer/Footer";
 import SideNavbar from "../../component/SideNavbar/SideNavbar";
-import {faHome, faRocket, faAngleDoubleDown} from "@fortawesome/free-solid-svg-icons";
+import {faHome, faRocket, faAngleDoubleDown, faBullseye} from "@fortawesome/free-solid-svg-icons";
+import IntroductionSection from "../../section/IntroductionSection/IntroductionSection";
 
 function LandingPage() {
   const navbarLinks = [
     {id: 1, title: "Home", color: [24, 144, 255], icon: faHome, target: "homeSection"},
+    {id: 1, title: "Cel", color: [113, 46, 206], icon: faBullseye, target: "IntroSection"},
     {id: 2, title: "Eksperyment", color: [34, 38, 123], icon: faRocket, target: "experimentSection"},
     {id: 3, title: "Footer", color: [24, 144, 255], icon: faAngleDoubleDown, target: "footerSection"},
   ]
@@ -16,6 +18,7 @@ function LandingPage() {
       <>
           <SideNavbar navbarLinks={navbarLinks}/>
           <HomeSection/>
+          <IntroductionSection />
           <Experiment/>
           <Footer/>
       </>
